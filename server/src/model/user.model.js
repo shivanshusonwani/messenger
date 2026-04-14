6a8a251f574e61e36a60c4c9d7a4b2b63181b3c3
ShivanshuSonwani <shivanshu.ed@gmail.com>
2026-04-14 13:25:21 +0530

    create user model schema

---
+++ server/src/model/user.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 6,
+		},
+		avatar: {
+			type: String,
+			default: "",
+		},
+	},
+	{ timestamps: true },
+);
+
+const User = model("User", userSchema);
+export default User;
