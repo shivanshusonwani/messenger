5ea5f4383dbf7d1f64890a924666a8b4be4e6823
ShivanshuSonwani <shivanshu.ed@gmail.com>
2026-04-14 13:17:28 +0530

    add health check route

--- server/src/app.js
+++ server/src/app.js
@@ -2,4 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.get("/health", (req, res) => {
+	res.status(200).json({
+		success: true,
+		message: "Server is running",
+	});
+});
+
 export default app;
